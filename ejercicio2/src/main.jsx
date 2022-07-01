4618756a6bb689f733877686e4a16e326f084c22
Fernando Arellano <farellano0@ucol.mx>
2022-07-01 09:34:22 -0500

    refactorización de main.jsx

--- ejercicio2/src/main.jsx
+++ ejercicio2/src/main.jsx
@@ -8,13 +8,28 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [all, setAll] = useState(0);
 
   //Increments
-  const incrementByOneGood = () => {setGood(good + 1), setAll(all + 1)};
-  const incrementByOneNeutral = () => {setNeutral(neutral + 1), setAll(all + 1)};
-  const incrementByOneBad = () => {setBad(bad + 1), setAll(all + 1)};
+  const incrementByOneGood = () => {setGood(good + 1)};
+  const incrementByOneNeutral = () => {setNeutral(neutral + 1)};
+  const incrementByOneBad = () => {setBad(bad + 1)};
 
+  //Statistics
+  const Statistics = (props) => {
+    const {good, neutral, bad} = props;
+    const total = good + neutral + bad;
+    const score = good - bad;
+    const average = score / total || 0;
+    const positive = good / total || 0;
+
+    return (
+      <div>
+        <p>All: {total}</p>
+        <p>Average: {average}</p>
+        <p>Positive: {positive}</p>
+      </div>
+    )
+  }
 
   return (
     <div>
@@ -30,9 +45,7 @@ const App = () => {
           <p>Good: {good}</p>
           <p>Neutral: {neutral}</p>
           <p>Bad: {bad}</p>
-          <p>Total de retroalimentación: {all}</p>
-          <p>Puntucación promedio: {(good - bad) / all}</p>
-          <p>Porcentaje de comentarios positivos: {good/all*100} %</p>
+          <Statistics good={good} neutral={neutral} bad={bad}/>
         </div>
       </div>  
     </div>
